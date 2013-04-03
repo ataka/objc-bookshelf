5343fd799c060d491ea975d99c2342bdf9f737bc
ataka <masayuki.ataka@gmail.com>
2013-04-03 12:35:42 +0900

    minimal Objective-C source code, but segmentation fault...
    I can't understand!!
    
    Signed-off-by: ataka <masayuki.ataka@gmail.com>

--- main.m
+++ main.m
@@ -1,9 +1,30 @@
 #import <objc/Object.h>
 #import <stdio.h>
 
+@interface Point : Object
+{
+        int x;
+}
+-(id) setX :(int)x;
+-(int) getX;
+@end
+
+@implementation Point
+-(id) setX :(int)x
+{
+  self->x = x;
+  return self;
+}
+-(int) getX {
+  return x;
+}
+@end
+
 int main(void)
 {
-  printf("Hello, World!\n");
+  id point = [Point alloc];
+  [point setX: 10];
+  printf("Point X is %d\n.", [point getX]);
 
   return 0;
 }
