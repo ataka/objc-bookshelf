c07d66b4fd348474bbab4338dc03db19fa0cd337
ataka <masayuki.ataka@gmail.com>
2013-04-08 10:53:54 +0900

    Access property with dot style instead of message style.
    Signed-off-by: ataka <masayuki.ataka@gmail.com>

--- main.m
+++ main.m
@@ -11,30 +11,30 @@ int main(void)
   Bookshelf* niigata = [[Bookshelf alloc] initWithPlace:@"Home at Niigata"];
 
   Book* book1 = [Book alloc];
-  [book1 setTitle: @"Foundation"];
-  [book1 setAuthor: @"Isaac Asimov"];
+  book1.title  = @"Foundation";
+  book1.author = @"Isaac Asimov";
 
   Book* book2 = [Book alloc];
-  [book2 setTitle: @"Foundation and Empire"];
-  [book2 setAuthor: @"Isaac Asimov"];
+  book2.title  = @"Foundation and Empire";
+  book2.author = @"Isaac Asimov";
 
   Book* book3 = [Book alloc];
-  [book3 setTitle: @"Second Foundation"];
-  [book3 setAuthor: @"Isaac Asimov"];
+  book3.title  = @"Second Foundation";
+  book3.author = @"Isaac Asimov";
 
   Book* book4 = [Book alloc];
-  [book4 setTitle: @"Ender's Game"];
-  [book4 setAuthor: @"Orson Scott Card"];
+  book4.title  = @"Ender's Game";
+  book4.author = @"Orson Scott Card";
 
   [niigata addBook: book1];
   [niigata addBook: book2];
   [niigata addBook: book3];
   [niigata addBook: book4];
 
-  printf("at %s\n----\n", [[niigata place] UTF8String]);
-  for (Book* book in [niigata books]){
-    printf("Title: %s\n", [[book title] UTF8String]);
-    printf("Author: %s\n--\n", [[book author] UTF8String]);
+  printf("at %s\n----\n", [niigata.place UTF8String]);
+  for (Book* book in niigata.books){
+    printf("Title: %s\n", [book.title UTF8String]);
+    printf("Author: %s\n--\n", [book.author UTF8String]);
   }
 
   [pool drain];
